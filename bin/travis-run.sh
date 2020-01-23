caebfe6e0705550d0b1448ce3a75eb973b6b0203
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-01-23 10:49:16 +0000

    Fix travis

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,14 +1,19 @@
 #!/bin/sh -e
+set -ex
 
-echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-sudo service jetty restart
+flake8 --version
+# stop the build if there are Python syntax errors or undefined names
+flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan,{{ project }}
 
 nosetests --ckan \
           --nologcapture \
           --with-pylons=subdir/test.ini \
           --with-coverage \
-          --cover-package=ckanext.emailasusername \
+          --cover-package=ckanext.ytp.request \
           --cover-inclusive \
           --cover-erase \
-          --cover-tests
+          --cover-tests \
+          ckanext/ytp/request
+
+# strict linting
+flake8 . --count --max-line-length=127 --statistics --exclude ckan,{{ project }}
