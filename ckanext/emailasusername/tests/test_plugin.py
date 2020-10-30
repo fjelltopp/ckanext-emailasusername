f8f398b088b02daae1379f914fc452b846cb65df
Jyri Soppela <jyri@fjelltopp.org>
2020-10-30 17:04:56 +0200

    migrate plugin tests to pytest

--- ckanext/emailasusername/tests/test_plugin.py
+++ ckanext/emailasusername/tests/test_plugin.py
@@ -6,29 +6,13 @@ import ckan.tests.factories
 from ckan.tests import helpers
 import ckanext.emailasusername.plugin as plugin
 import logging
-import unittest
+import pytest
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-class ModuleTests(unittest.TestCase):
-
-    @classmethod
-    def setup_class(self):
-        log.warning("Setting up class")
-        ckan.plugins.load('emailasusername')
-
-    @classmethod
-    def teardown_class(self):
-        log.warning("Tearing down class")
-        ckan.plugins.unload('emailasusername')
-        helpers.reset_db()
-
-    def teardown(self):
-        log.warning("Tearing down function")
-        helpers.reset_db()
-        assert False
+class TestEmails(object):
 
     def test_user_emails_match(self):
         key = ('email',)
@@ -42,14 +26,14 @@ class ModuleTests(unittest.TestCase):
 
         # Test that function works for matching emails
         plugin.user_emails_match(key, data, errors, context)
-        self.assertEquals(errors, {('email',): []})
-        self.assertEquals(data[('email', )], data[('email1',)])
+        assert errors == {('email',): []}
+        assert data[('email', )] == data[('email1',)]
 
         # Test that the function works for non-matching emails
         data[('email2',)] = 'tes@test.com'
         errors = {('email',): []}
         plugin.user_emails_match(key, data, errors, context)
-        self.assertTrue(len(errors[('email',)]) > 0)
+        assert len(errors[('email',)]) > 0
 
     def test_user_both_emails_entered(self):
         key = ('email',)
@@ -63,30 +47,30 @@ class ModuleTests(unittest.TestCase):
 
         # Test that the function works for correct input
         plugin.user_both_emails_entered(key, data, errors, context)
-        self.assertTrue(len(errors[('email',)]) == 0)
+        assert len(errors[('email',)]) == 0
 
         # Test that the function works for missing input
         data[('email1',)] = ''
         errors = {('email',): []}
         plugin.user_both_emails_entered(key, data, errors, context)
-        self.assertTrue(len(errors[('email',)]) == 1)
+        assert len(errors[('email',)]) == 1
 
         data[('email1',)] = None
         errors = {('email',): []}
         plugin.user_both_emails_entered(key, data, errors, context)
         log.warning(errors)
-        self.assertTrue(len(errors[('email',)]) == 1)
+        assert len(errors[('email',)]) == 1
 
         data[('email2',)] = 'test@test.com'
         data[('email2',)] = ''
         errors = {('email',): []}
         plugin.user_both_emails_entered(key, data, errors, context)
-        self.assertTrue(len(errors[('email',)]) == 1)
+        assert len(errors[('email',)]) == 1
 
         data[('email2',)] = None
         errors = {('email',): []}
         plugin.user_both_emails_entered(key, data, errors, context)
-        self.assertTrue(len(errors[('email',)]) == 1)
+        assert len(errors[('email',)]) == 1
 
     def test_email_exists(self):
         # Test email exists validator for valid data
@@ -95,7 +79,7 @@ class ModuleTests(unittest.TestCase):
         errors = {('email',): []}
         context = {}
         plugin.email_exists(key, data, errors, context)
-        self.assertEquals(errors, {('email',): []})
+        assert errors, {('email',): []}
 
         # Test email exists validator for invalid data
         # i.e. a pre-existing account already exists with the given email
@@ -105,39 +89,32 @@ class ModuleTests(unittest.TestCase):
         )
         data = {('email',): test_user_dict['email']}
         plugin.email_exists(key, data, errors, context)
-        self.assertTrue(len(errors[('email',)]) == 1)
+        assert len(errors[('email',)]) == 1
 
     def test_emailasusername_new_user_schema(self):
 
         schema = ckan.logic.schema.user_new_form_schema()
-        self.assertIn('email', schema)
-        self.assertIn('email1', schema)
-        self.assertIn('email2', schema)
-        self.assertIn('password1', schema)
-        self.assertIn('password2', schema)
+        assert 'email' in schema
+        assert 'email1' in schema
+        assert 'email2' in schema
+        assert 'password1' in schema
+        assert 'password2' in schema
 
         def get_validator_names(validator_list):
             return list(map(lambda f: f.__name__, validator_list))
 
         email1_validators = get_validator_names(schema['email1'])
-        self.assertIn('user_emails_match', email1_validators)
-        self.assertIn('user_both_emails_entered', email1_validators)
+        assert 'user_emails_match' in email1_validators
+        assert 'user_both_emails_entered' in email1_validators
 
         email_validators = get_validator_names(schema['email'])
-        self.assertNotIn('not_empty', email_validators)
+        assert 'not_empty' not in email_validators
 
         default_schema = ckan.logic.schema.default_user_schema()
         del default_schema['email']
 
         # Check that all the other default schema fields remain untouched
         for key, value in default_schema.items():
-            self.assertIn(
-                key, schema,
-                "Key from default user schema not found: {}".format(key)
-            )
-            self.assertEquals(
-                value, schema[key],
-                "Value from default user schema changed: {} | {} | {}".format(
-                    key, value, schema[key]
-                )
-            )
+            assert key in schema
+            assert value in schema[key]
+
