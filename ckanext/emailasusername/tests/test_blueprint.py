c63b06ca2da5d9192fd813c365fba5a36de8e422
Jyri Soppela <jyri@fjelltopp.org>
2020-10-30 14:20:58 +0200

    migrate blueprint tests to pytest

--- ckanext/emailasusername/tests/test_blueprint.py
+++ ckanext/emailasusername/tests/test_blueprint.py
@@ -1,3 +1,6 @@
+import pydevd_pycharm
+pydevd_pycharm.settrace('host.docker.internal', port=9876, stdoutToServer=True, stderrToServer=True)
+
 """Tests for plugin.py."""
 import ckan.plugins
 import ckan.model
@@ -7,31 +10,27 @@ import six
 from ckan.lib.helpers import url_for
 from ckan.tests import helpers
 import ckanext.emailasusername.blueprint as blueprint
+from ckan.lib.mailer import MailerException
 import logging
-import unittest
+import pytest
 import mock
-from ckan.tests.helpers import submit_and_follow
+#from ckan.tests.helpers import submit_and_follow
 import ckan.plugins as plugins
 
 log = logging.getLogger(__name__)
 
+@pytest.fixture
+def initdb():
+    ckan.model.Session.remove()
+    ckan.model.Session.configure(bind=ckan.model.meta.engine)
 
-class ModuleTests(unittest.TestCase):
-
-    @classmethod
-    def setup_class(self):
-        log.warning("Setting up class")
-        ckan.plugins.load('emailasusername')
-
-    @classmethod
-    def teardown_class(self):
-        log.warning("Tearing down class")
-        ckan.plugins.unload('emailasusername')
-        helpers.reset_db()
-
-    def teardown(self):
-        log.warning("Tearing down function")
-        helpers.reset_db()
+#@pytest.mark.usefixtures(u'initdb')
+@pytest.mark.usefixtures(u'clean_db')
+@pytest.mark.ckan_config(u'ckan.plugins', u'emailasusername')
+@pytest.mark.usefixtures(u'with_plugins')
+@pytest.mark.usefixtures(u'with_request_context')
+@pytest.mark.usefixtures(u'mail_server')
+class ModuleTests(object):
 
     @mock.patch('ckanext.emailasusername.blueprint.h.flash_error')
     def test_user_by_username_or_email(self, flash_mock):
@@ -41,24 +40,24 @@ class ModuleTests(unittest.TestCase):
 
         # Test getting user by username
         user_obj = blueprint.user_by_username_or_email(username)
-        self.assertEquals(getattr(user_obj, 'name'), username)
-        self.assertEquals(getattr(user_obj, 'email'), email)
+        assert getattr(user_obj, 'name') == username
+        assert getattr(email, 'name') == email
 
         # Test getting user by email
         user_obj = blueprint.user_by_username_or_email(email)
-        self.assertEquals(getattr(user_obj, 'name'), username)
-        self.assertEquals(getattr(user_obj, 'email'), email)
+        assert getattr(user_obj, 'name') == username
+        assert getattr(email, 'name') == email
 
         # Test trying to get a nonexistant user
         user_obj = blueprint.user_by_username_or_email('wrongname')
         flash_mock.assert_called()
-        self.assertEquals(user_obj, None)
+        assert user_obj is None
 
         # Test trying to get by email when multiple accounts have same email
         ckan.tests.factories.User(name='tester2', email=email)
         user_obj = blueprint.user_by_username_or_email(email)
         flash_mock.assert_called()
-        self.assertEquals(user_obj, None)
+        assert user_obj is None
 
 
 def _assert_in_body(string, response):
@@ -74,77 +73,84 @@ def _assert_login_page_displayed(response):
         assert input in login_form.fields
 
 
-def _assert_request_reset_page_displayed(response):
-    reset_form = response.forms[1]
-    assert reset_form.action == url_for('emailasusername.request_reset')
+# def _assert_request_reset_page_displayed(response):
+#     reset_form = response.forms[1]
+#     assert reset_form.action == url_for('emailasusername.request_reset')
 
-
-class TestRequestResetFunctional(helpers.FunctionalTestBase):
-    def setup(self):
-        helpers.reset_db()
-        self.app = helpers._get_test_app()
-        if not plugins.plugin_loaded(u'emailasusername'):
-            plugins.load(u'emailasusername')
-            plugin = plugins.get_plugin(u'emailasusername')
-            self.app.flask_app.register_extension_blueprint(plugin.get_blueprint())
-
-    def teardown(self):
-        ckan.plugins.unload('emailasusername')
+@pytest.mark.usefixtures(u'initdb')
+@pytest.mark.usefixtures(u'clean_db')
+@pytest.mark.ckan_config(u'ckan.plugins', u'emailasusername')
+@pytest.mark.usefixtures(u'with_plugins')
+@pytest.mark.usefixtures(u'with_request_context')
+@pytest.mark.usefixtures(u'mail_server')
+class TestRequestResetFunctional(object):
 
     @mock.patch('ckanext.emailasusername.blueprint.mailer.send_reset_link')
-    def test_request_reset_sends_reset_link_for_correct_username(self, mailer_mock):
+    def test_request_reset_sends_reset_link_for_correct_username(self, mailer_mock, app):
         # given:
         user = ckan.tests.factories.User()
+
         # when:
-        password_reset_form = self.app.get(url_for('user.request_reset')).forms[1]
-        password_reset_form['user'] = user['name']
-        test_response = submit_and_follow(self.app, password_reset_form, name='reset')
+        offset = url_for("user.request_reset")
+        test_response = app.post(
+            offset, data=dict(user=user["name"])
+        )
 
         # then:
         assert mailer_mock.called
         called_user = mailer_mock.call_args[0][0]
         assert called_user.id == user['id']
-        _assert_in_body("Please check your inbox for a reset code.", test_response)
-        _assert_login_page_displayed(test_response)
+        _assert_in_body("A reset link has been emailed to you", test_response)
 
     @mock.patch('ckanext.emailasusername.blueprint.mailer.send_reset_link')
-    def test_request_reset_sends_reset_link_for_correct_email(self, mailer_mock):
+    def test_request_reset_sends_reset_link_for_correct_email(self, mailer_mock, app):
         # given:
         user = ckan.tests.factories.User()
+
         # when:
-        password_reset_form = self.app.get(url_for('user.request_reset')).forms[1]
-        password_reset_form['user'] = user['email']
-        test_response = submit_and_follow(self.app, password_reset_form, name='reset')
+        offset = url_for("user.request_reset")
+        test_response = app.post(
+            offset, data=dict(user=user["email"])
+        )
 
         # then:
         assert mailer_mock.called
         called_user = mailer_mock.call_args[0][0]
         assert called_user.id == user['id']
-        _assert_in_body("Please check your inbox for a reset code.", test_response)
-        _assert_login_page_displayed(test_response)
+        _assert_in_body("A reset link has been emailed to you", test_response)
 
     @mock.patch('ckanext.emailasusername.blueprint.mailer.send_reset_link')
-    def test_request_reset_display_error_for_incorrect_email(self, mailer_mock):
+    def test_request_reset_display_error_for_incorrect_email(self, mailer_mock, app):
         # given:
         fake_email = 'fake@mail.com'
+
         # when:
-        password_reset_form = self.app.get(url_for('user.request_reset')).forms[1]
-        password_reset_form['user'] = fake_email
-        test_response = submit_and_follow(self.app, password_reset_form, name='reset')
+        offset = url_for("user.request_reset")
+        test_response = app.post(
+            offset, data=dict(user=fake_email)
+        )
 
         # then:
         assert not mailer_mock.called
-        _assert_in_body("No such user: {}".format(fake_email), test_response)
-        _assert_request_reset_page_displayed(test_response)
+        _assert_in_body("A reset link has been emailed to you", test_response)
+        #_assert_request_reset_page_displayed(test_response)
 
-    def test_request_reset_display_error_on_smtp_problem(self):
+    @mock.patch('ckanext.emailasusername.blueprint.mailer.send_reset_link')
+    def test_request_reset_display_error_on_smtp_problem(self, send_reset_link, app):
         # given:
         user = ckan.tests.factories.User()
+
         # when:
-        r = self.app.get(url_for('user.request_reset'))
-        password_reset_form = r.forms[1]
-        password_reset_form['user'] = user['name']
-        test_response = submit_and_follow(self.app, password_reset_form, name='reset')
+        offset = url_for("user.request_reset")
+        # This is the exception when the mailer is not configured:
+        send_reset_link.side_effect = MailerException(
+            'SMTP server could not be connected to: "localhost" '
+            "[Errno 111] Connection refused"
+        )
+        test_response = app.post(
+            offset, data=dict(user=user["name"])
+        )
+
         # then:
-        _assert_in_body("Could not send reset link:", test_response)
-        _assert_request_reset_page_displayed(test_response)
+        _assert_in_body("Error sending the email. Try again later or contact an administrator for help", test_response)
+        #_assert_request_reset_page_displayed(test_response)
