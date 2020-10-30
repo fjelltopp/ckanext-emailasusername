2c0343724e5c3afdcf9736d55f040a81bffb9083
Jyri Soppela <jyri@fjelltopp.org>
2020-10-30 17:08:54 +0200

    bring authenticator tests to coverage

--- ckanext/emailasusername/tests/test_authenticator.py
+++ ckanext/emailasusername/tests/test_authenticator.py
@@ -1,34 +1,27 @@
 """Tests for plugin.py."""
 import ckan.plugins
-import ckan.model
+import ckan.model as model
 import ckan.logic.schema
 import ckan.tests.factories
 from ckan.tests import helpers
 import ckanext.emailasusername.authenticator as authenticator
 import logging
-import unittest
+import pytest
 import mock
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+@pytest.fixture
+def initdb():
+    model.Session.remove()
+    model.Session.configure(bind=model.meta.engine)
+    model.init_tables()
 
-
-class ModuleTests(unittest.TestCase):
-
-    @classmethod
-    def setup_class(self):
-        log.warning("Setting up class")
-        ckan.plugins.load('emailasusername')
-
-    @classmethod
-    def teardown_class(self):
-        log.warning("Tearing down class")
-        ckan.plugins.unload('emailasusername')
-        helpers.reset_db()
-
-    def teardown(self):
-        log.warning("Tearing down function")
-        helpers.reset_db()
+@pytest.mark.usefixtures(u'initdb')
+@pytest.mark.usefixtures(u'clean_db')
+@pytest.mark.ckan_config(u'ckan.plugins', u'ytp_request')
+@pytest.mark.usefixtures(u'with_plugins')
+@pytest.mark.usefixtures(u'with_request_context')
+@pytest.mark.usefixtures(u'mail_server')
+class TestAuthenticator(object):
 
     @mock.patch('ckanext.emailasusername.blueprint.h.flash_error')
     def test_authenticate(self, flash_mock):
@@ -41,19 +34,23 @@ class ModuleTests(unittest.TestCase):
             email='test@ckan.org',
             password=identity['password']
         )
+
         # Test that a correct login returns the username
         response = auth.authenticate({}, identity)
-        self.assertEquals(response, 'tester')
+        assert response == 'tester'
+        #self.assertEquals(response, 'tester')
 
         # Test that a correct email based login returns the username
         identity['login'] = 'test@ckan.org'
         response = auth.authenticate({}, identity)
-        self.assertEquals(response, 'tester')
+        assert response == 'tester'
+        #self.assertEquals(response, 'tester')
 
         # Test that an incorrect email based login fails login
         identity['login'] = 'test@ckan-wrong.org'
         response = auth.authenticate({}, identity)
-        self.assertEquals(response, None)
+        assert response is None
+        #self.assertEquals(response, None)
         flash_mock.assert_not_called()
 
         # Test that login fails when two accounts registered with email exists
@@ -65,11 +62,13 @@ class ModuleTests(unittest.TestCase):
         )
         identity['login'] = 'test@ckan.org'
         response = auth.authenticate({}, identity)
-        self.assertEquals(response, None)
+        assert response is None
+        #self.assertEquals(response, None)
         flash_mock.assert_not_called()
 
         # Test that an incorrect password fails login
         identity['password'] += '!'
         response = auth.authenticate({}, identity)
-        self.assertEquals(response, None)
+        assert response is None
+        #self.assertEquals(response, None)
         flash_mock.assert_not_called()
