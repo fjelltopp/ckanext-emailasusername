92432812f6fac1bcde3a84b6aeb822f4b251a4ec
Jonathan Berry <jonathan.s.berry@googlemail.com>
2021-05-25 08:46:37 +0100

    ADX-658 refactor tests.

--- ckanext/emailasusername/tests/test_plugin.py
+++ ckanext/emailasusername/tests/test_plugin.py
@@ -16,34 +16,6 @@ log.setLevel(logging.DEBUG)
 @pytest.mark.ckan_config(u'ckan.plugins', u'emailasusername')
 @pytest.mark.usefixtures(u'with_plugins')
 @pytest.mark.usefixtures(u'with_request_context')
-class TestSearchUsersByEmail(object):
-
-    @pytest.mark.ckan_config(u'emailasusername.search_by_email', u'')
-    def test_search_by_email_without_config(self):
-        identity = {'name': 'login', 'email': 'test@ckan.org', 'password': 'password'}
-        ckan.tests.factories.User(**identity)
-        response = ckan.tests.helpers.call_action('user_autocomplete', {}, q=identity['email'])
-        assert not response
-
-    @pytest.mark.ckan_config(u'emailasusername.search_by_email', u'True')
-    def test_search_by_full_email(self):
-        identity = {'name': 'login', 'email': 'test@ckan.org', 'password': 'password'}
-        ckan.tests.factories.User(**identity)
-        response = ckan.tests.helpers.call_action('user_autocomplete', {}, q=identity['email'])
-        assert len(response) == 1
-
-    @pytest.mark.ckan_config(u'emailasusername.search_by_email', u'True')
-    def test_search_by_partial_email(self):
-        identity = {'name': 'login', 'email': 'test@ckan.org', 'password': 'password'}
-        ckan.tests.factories.User(**identity)
-        response = ckan.tests.helpers.call_action('user_autocomplete', {}, q=identity['email'][:-1])
-        assert not response
-
-
-@pytest.mark.usefixtures(u'clean_db')
-@pytest.mark.ckan_config(u'ckan.plugins', u'emailasusername')
-@pytest.mark.usefixtures(u'with_plugins')
-@pytest.mark.usefixtures(u'with_request_context')
 class TestEmails(object):
 
     def test_user_emails_match(self):
