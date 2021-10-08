99bc3e70f75dc6356b411d4c928ceb39af412f3b
manoj-nathwani <manoj_1993@hotmail.com>
2021-10-08 12:53:31 +0000

    skip user email confirmation feature

--- ckanext/emailasusername/helpers.py
+++ ckanext/emailasusername/helpers.py
@@ -1,8 +1,16 @@
 from ckan.plugins import toolkit as toolkit
 
 
-def get_auto_generate_username_from_fullname():
+def _get_extension_env(env_variable, default_value):
     return toolkit.config.get(
-        'ckanext.emailasusername.auto_generate_username_from_fullname',
-        False
+        'ckanext.emailasusername.{}'.format(env_variable),
+        default_value
     )
+
+
+def get_auto_generate_username_from_fullname():
+    return _get_extension_env('auto_generate_username_from_fullname', False)
+
+
+def get_skip_user_email_input_confirmation():
+    return _get_extension_env('skip_user_email_input_confirmation', False)
