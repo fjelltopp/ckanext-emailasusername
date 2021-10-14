f668d8948ea247992c6beb432f2ca9ba9277c1b0
Tomasz Sabała <tomek@fjelltopp.org>
2021-10-14 11:30:31 +0200

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.1',
+    version='1.0.0',
     # python 2.7+, 3.6+
     python_requires='>2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     description='''Allows users to login with email as well as username.''',
