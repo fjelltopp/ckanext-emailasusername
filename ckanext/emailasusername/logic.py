92a29ddf265622f2769cf6a9c0850ec0c0112443
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-07-21 12:08:07 +0100

    OHT-60 Refactor using copy of privte function

--- ckanext/emailasusername/logic.py
+++ ckanext/emailasusername/logic.py
@@ -6,7 +6,8 @@ from sqlalchemy import func
 import ckan.plugins.toolkit as toolkit
 import ckan.plugins as p
 from ckanext.emailasusername.interfaces import IEmailAsUsername
-from ckan.logic.action.create import _get_random_username_from_email
+import re
+import random
 
 _check_access = logic.check_access
 
@@ -86,13 +87,44 @@ def user_create(next_action, context, data_dict):
     if not data_dict.get('email'):
         raise toolkit.ValidationError(toolkit._("Must specify either a name or an email"))
 
-    data_dict['name'] = _generate_username(data_dict['email'])
+    email = data_dict['email']
+    username = _get_random_username_from_email(email, context['model'])
+
+    for plugin in p.PluginImplementations(IEmailAsUsername):
+        username = plugin.generate_username(email, username)
+
+    data_dict['name'] = username
 
     return next_action(context, data_dict)
 
 
-def _generate_username(email):
-    username = _get_random_username_from_email(email)
-    for plugin in p.PluginImplementations(IEmailAsUsername):
-        username = plugin.generate_username(email, username)
-    return username
+def _get_random_username_from_email(email, model):
+    """
+    This function is copied from a CKAN core private function:
+        ckan.logic.action.create._get_random_username_from_email
+    Github permalink:
+        https://github.com/ckan/ckan/blob/0a596b8394dbf9582902853ad91450d2c0d7959b/ckan/logic/action/create.py#L1102-L1116
+
+    The function has been deployed and used across a plethora of CKAN
+    instances, which is why we are adopting it here.
+
+    WARNING: This logic reveals part of the user's email address
+    as their username.  Fjelltopp recommends overriding this logic
+    for public CKAN instances. Use the IEmailAsUsername plugin
+    interface to do this.
+    """
+
+    localpart = email.split('@')[0]
+    cleaned_localpart = re.sub(r'[^\w]', '-', localpart).lower()
+
+    # if we can't create a unique user name within this many attempts
+    # then something else is probably wrong and we should give up
+    max_name_creation_attempts = 100
+
+    for i in range(max_name_creation_attempts):
+        random_number = random.SystemRandom().random() * 10000
+        name = '%s-%d' % (cleaned_localpart, random_number)
+        if not model.User.get(name):
+            return name
+
+    return cleaned_localpart
